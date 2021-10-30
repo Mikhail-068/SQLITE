1bb2a9aee72a79d4afb7bbbb6be78d29218e8e82
Mike <89531276499nokia@gmail.com>
2021-10-30 19:36:10 +0300

    Добавили данные в `garage`

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('Будем работать с SQLITE')
+import sqlite3
+from config import *
+
+connection = sqlite3.connect('Diesel.db')
+cur = connection.cursor()
+
+cur.execute("CREATE TABLE IF NOT EXISTS users ("
+            "id INTEGER AUTO_INCREMENT,"
+            "surname TEXT,"
+            "money INTEGER DEFAULT 0,"
+            "PRIMARY KEY(id)"
+            ")")
+connection.commit()
+
+cur.execute("CREATE TABLE IF NOT EXISTS garage ("
+            "id INTEGER AUTO_INCREMENT,"
+            "data_stopped DATE,"
+            "liters INTEGER DEFAULT 0,"
+            "id_driver INTEGER,"
+            "PRIMARY KEY(id)"
+            ")")
+connection.commit()
+
+cur.executemany("INSERT INTO `garage` (data_stopped, liters, id_driver) VALUES" \
+            "(?, ?, ?)", (list_garage))
+connection.commit()
+connection.close()
