18020107d5a16a2febe484586cf2f3b04c7fcebe
Mike <89531276499nokia@gmail.com>
2021-10-30 22:07:13 +0300

    users ver 02

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import sqlite3
 from config import *
 
+def del_(x):
+    cur.execute(f"DROP TABLE IF EXISTS {x}")
+def add_(x):
+    cur.execute("INSERT INTO users (surname) VALUES (?)", (x,))
+
 connection = sqlite3.connect('Diesel.db')
 cur = connection.cursor()
 
+# del_('users')
 cur.execute("CREATE TABLE IF NOT EXISTS users ("
-            "id INTEGER AUTO_INCREMENT,"
-            "surname TEXT,"
-            "money INTEGER DEFAULT 0,"
+            "id INT AUTO_INCREMENT,"
+            "surname VARCHAR(15),"
+            "money INT DEFAULT 0,"
             "PRIMARY KEY(id)"
             ")")
-connection.commit()
-
-cur.execute("CREATE TABLE IF NOT EXISTS garage ("
-            "id INTEGER AUTO_INCREMENT,"
-            "data_stopped DATE,"
-            "liters INTEGER DEFAULT 0,"
-            "id_driver INTEGER,"
+cur.execute("CREATE TABLE IF NOT EXISTS `garage` ("
+            "`id` INT AUTO_INCREMENT,"
+            "`data_stopped` DATE,"
+            "`liters` INT DEFAULT 0,"
+            "`id_driver` INT,"
             "PRIMARY KEY(id)"
-            ")")
-connection.commit()
+            ")"
+            )
 
+for i in range(len(list_users)):
+    add_(users[i])
+    connection.commit()
 
-cur.executemany("INSERT INTO users (surname, money) VALUES (?, ?)", (list_users))
-connection.commit()
+connection.close()
